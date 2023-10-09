a38ec0f61d55d9f3226809db9f169c61538610f4
Dmitry <iamdimonis@gmail.com>
2023-10-09 21:59:53 +0300

    simple exceptions added

--- src/ru/vsu/csf/Main.java
+++ src/ru/vsu/csf/Main.java
@@ -29,145 +29,183 @@ public class Main {
     }
 
     static void createPatient(PatientController patientController) {
-        System.out.println("Введите ФИО, возраст, пол в формате: Иванов Иван Иванович 18 М");
-        in.nextLine();
-        String[] s = in.nextLine().split(" ");
-        patientController.createPatient(s[1], s[0], s[2], Integer.parseInt(s[3]), s[4].equals("М") ? Sex.MALE : Sex.FEMALE);
-        System.out.println("Успешно!");
+        try {
+            System.out.println("Введите ФИО, возраст, пол в формате: Иванов Иван Иванович 18 М");
+            in.nextLine();
+            String[] s = in.nextLine().split(" ");
+            patientController.createPatient(s[1], s[0], s[2], Integer.parseInt(s[3]), s[4].equals("М") ? Sex.MALE : Sex.FEMALE);
+            System.out.println("Успешно!");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ввод некорректен!");
+        }
     }
 
     static void createDepartment(DepartmentController departmentController) {
-        System.out.println("Введите название отделения в формате: Хирургия");
-        String[] s = in.next().split(" ");
-        departmentController.createDepartment(s[0]);
-        System.out.println("Успешно!");
+        try {
+            System.out.println("Введите название отделения в формате: Хирургия");
+            String[] s = in.next().split(" ");
+            departmentController.createDepartment(s[0]);
+            System.out.println("Успешно!");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ввод некорректен!");
+        }
     }
 
     static void showDepartments(DepartmentController departmentController) {
+        if (departmentController.showDepartments().size() == 0) {
+            System.out.println("Нет отделений");
+        }
         for (Department department : departmentController.showDepartments()) {
             System.out.println(department);
         }
     }
 
     static void showPatients(PatientController patientController) {
+        if (patientController.showPatients().size() == 0) {
+            System.out.println("Нет пациентов");
+        }
         for (Patient patient : patientController.showPatients()) {
             System.out.println(patient);
         }
     }
 
     static void addPatientToDepartment(DepartmentController departmentController) {
-        System.out.println("Если хотите добавить пациента в отделение по его id, введите 1, если по названию, введите 2");
-        String s = in.next();
-        if (s.equals("1")) {
-            System.out.println("Введите id отделения, затем id пациента в формате: 3 2");
-            String s1 = in.next();
-            String s2 = in.next();
-            departmentController.addPatientByIds(Integer.parseInt(s1), Integer.parseInt(s2));
-        } else if (s.equals("2")) {
-            System.out.println("Введите название отделения, затем id пациента в формате: Хирургия 2");
-            String s1 = in.next();
-            String s2 = in.next();
-            departmentController.addPatientByPatientId(s1, Integer.parseInt(s2));
+        try {
+            System.out.println("Если хотите добавить пациента в отделение по его id, введите 1, если по названию, введите 2");
+            String s = in.next();
+            if (s.equals("1")) {
+                System.out.println("Введите id отделения, затем id пациента в формате: 3 2");
+                String s1 = in.next();
+                String s2 = in.next();
+                departmentController.addPatientByIds(Integer.parseInt(s1), Integer.parseInt(s2));
+            } else if (s.equals("2")) {
+                System.out.println("Введите название отделения, затем id пациента в формате: Хирургия 2");
+                String s1 = in.next();
+                String s2 = in.next();
+                departmentController.addPatientByPatientId(s1, Integer.parseInt(s2));
+            }
+            System.out.println("Успешно!");
+        }  catch (Exception e) {
+            throw new IllegalArgumentException("Ввод некорректен!");
         }
-        System.out.println("Успешно!");
     }
 
     static void removePatientFromDepartment(DepartmentController departmentController) {
-        System.out.println("Если хотите удалить пациента из отделения по его id, введите 1, если по названию, введите 2");
-        String s = in.next();
-        if (s.equals("1")) {
-            System.out.println("Введите id отделения, затем id пациента в формате: 3 2");
-            String s1 = in.next();
-            String s2 = in.next();
-            departmentController.removePatientByIds(Integer.parseInt(s1), Integer.parseInt(s2));
-        } else if (s.equals("2")) {
-            System.out.println("Введите название отделения, затем id пациента в формате: Хирургия 2");
-            String s1 = in.next();
-            String s2 = in.next();
-            departmentController.removePatientByPatientId(s1, Integer.parseInt(s2));
+        try {
+            System.out.println("Если хотите удалить пациента из отделения по его id, введите 1, если по названию, введите 2");
+            String s = in.next();
+            if (s.equals("1")) {
+                System.out.println("Введите id отделения, затем id пациента в формате: 3 2");
+                String s1 = in.next();
+                String s2 = in.next();
+                departmentController.removePatientByIds(Integer.parseInt(s1), Integer.parseInt(s2));
+            } else if (s.equals("2")) {
+                System.out.println("Введите название отделения, затем id пациента в формате: Хирургия 2");
+                String s1 = in.next();
+                String s2 = in.next();
+                departmentController.removePatientByPatientId(s1, Integer.parseInt(s2));
+            }
+            System.out.println("Успешно!");
+        }  catch (Exception e) {
+            throw new IllegalArgumentException("Ввод некорректен!");
         }
-        System.out.println("Успешно!");
     }
 
     static void updateDepartmentName(DepartmentController departmentController) {
-        System.out.println("Если хотите обновить название отделения по его id, введите 1, если по названию, введите 2");
-        String s = in.next();
-        if (s.equals("1")) {
-            System.out.println("Введите id отделения, затем новое название в формате: 3 Хирургия");
-            String s1 = in.next();
-            String s2 = in.next();
-            departmentController.updateDepartmentById(Integer.parseInt(s1), s2);
-        } else if (s.equals("2")) {
-            System.out.println("Введите название отделения, затем новое название в формате: Неврология Хирургия");
-            String s1 = in.next();
-            String s2 = in.next();
-            departmentController.updateDepartmentByName(s1, s2);
+        try {
+            System.out.println("Если хотите обновить название отделения по его id, введите 1, если по названию, введите 2");
+            String s = in.next();
+            if (s.equals("1")) {
+                System.out.println("Введите id отделения, затем новое название в формате: 3 Хирургия");
+                String s1 = in.next();
+                String s2 = in.next();
+                departmentController.updateDepartmentById(Integer.parseInt(s1), s2);
+            } else if (s.equals("2")) {
+                System.out.println("Введите название отделения, затем новое название в формате: Неврология Хирургия");
+                String s1 = in.next();
+                String s2 = in.next();
+                departmentController.updateDepartmentByName(s1, s2);
+            }
+            System.out.println("Успешно!");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ввод некорректен!");
         }
-        System.out.println("Успешно!");
     }
     static void removePatient(PatientController patientController) {
-        System.out.println("Если хотите удалить пациента по его id, введите 1, если по полному имени, введите 2");
-        String s = in.next();
-        if (s.equals("1")) {
-            System.out.println("Введите id пациента в формате: 3");
-            String s1 = in.next();
-            patientController.removePatientById(Integer.parseInt(s1));
-        } else if (s.equals("2")) {
-            System.out.println("Введите полное имя пациента в формате: Иванов Иван Иванович");
-            in.nextLine();
-            String[] s1 = in.nextLine().split(" ");
-            patientController.removePatientByFullName(s1[1], s1[0], s1[2]);
+        try {
+            System.out.println("Если хотите удалить пациента по его id, введите 1, если по полному имени, введите 2");
+            String s = in.next();
+            if (s.equals("1")) {
+                System.out.println("Введите id пациента в формате: 3");
+                String s1 = in.next();
+                patientController.removePatientById(Integer.parseInt(s1));
+            } else if (s.equals("2")) {
+                System.out.println("Введите полное имя пациента в формате: Иванов Иван Иванович");
+                in.nextLine();
+                String[] s1 = in.nextLine().split(" ");
+                patientController.removePatientByFullName(s1[1], s1[0], s1[2]);
+            }
+            System.out.println("Успешно!");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ввод некорректен!");
         }
-        System.out.println("Успешно!");
     }
 
     static void removeDepartment(DepartmentController departmentController) {
-        System.out.println("Если хотите удалить отделение по его id, введите 1, если по названию, введите 2");
-        String s = in.next();
-        if (s.equals("1")) {
-            System.out.println("Введите id отделения в формате: 3");
-            String s1 = in.next();
-            departmentController.removeDepartmentById(Integer.parseInt(s1));
-        } else if (s.equals("2")) {
-            System.out.println("Введите название отделения в формате: Хирургия");
-            String s1 = in.next();
-            departmentController.removeDepartmentByName(s1);
+        try {
+            System.out.println("Если хотите удалить отделение по его id, введите 1, если по названию, введите 2");
+            String s = in.next();
+            if (s.equals("1")) {
+                System.out.println("Введите id отделения в формате: 3");
+                String s1 = in.next();
+                departmentController.removeDepartmentById(Integer.parseInt(s1));
+            } else if (s.equals("2")) {
+                System.out.println("Введите название отделения в формате: Хирургия");
+                String s1 = in.next();
+                departmentController.removeDepartmentByName(s1);
+            }
+            System.out.println("Успешно!");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ввод некорректен!");
         }
-        System.out.println("Успешно!");
     }
 
     static void updatePatient(PatientController patientController) {
-        System.out.println("Если хотите обновить это по id пациента, введите 1, если по полному имени, введите 2");
-        String s1 = in.next();
-        System.out.println("Если хотите обновить имя введите 1, если фамилию, введите 2, отчество - 3, возраст - 4, пол - 5");
-        String s = in.next();
-        if (s1.equals("1")) {
-            System.out.println("Введите id пациента в формате: 3");
-            String s2 = in.next();
-            System.out.println("Введите новое значение выбранного поля");
-            String s3 = in.next();
-            switch (s) {
-                case "1" -> patientController.updateFirstNameById(Integer.parseInt(s2), s3);
-                case "2" -> patientController.updateLastNameById(Integer.parseInt(s2), s3);
-                case "3" -> patientController.updatePatronymicById(Integer.parseInt(s2), s3);
-                case "4" -> patientController.updateAgeById(Integer.parseInt(s2), Integer.parseInt(s3));
-                case "5" -> patientController.updateSexById(Integer.parseInt(s2), s3.equals("М") ? Sex.MALE : Sex.FEMALE);
+        try {
+            System.out.println("Если хотите обновить это по id пациента, введите 1, если по полному имени, введите 2");
+            String s1 = in.next();
+            System.out.println("Если хотите обновить имя введите 1, если фамилию, введите 2, отчество - 3, возраст - 4, пол - 5");
+            String s = in.next();
+            if (s1.equals("1")) {
+                System.out.println("Введите id пациента в формате: 3");
+                String s2 = in.next();
+                System.out.println("Введите новое значение выбранного поля");
+                String s3 = in.next();
+                switch (s) {
+                    case "1" -> patientController.updateFirstNameById(Integer.parseInt(s2), s3);
+                    case "2" -> patientController.updateLastNameById(Integer.parseInt(s2), s3);
+                    case "3" -> patientController.updatePatronymicById(Integer.parseInt(s2), s3);
+                    case "4" -> patientController.updateAgeById(Integer.parseInt(s2), Integer.parseInt(s3));
+                    case "5" -> patientController.updateSexById(Integer.parseInt(s2), s3.equals("М") ? Sex.MALE : Sex.FEMALE);
+                }
+            } else if (s1.equals("2")) {
+                System.out.println("Введите полное имя пациента в формате: Иванов Иван Иванович");
+                in.nextLine();
+                String[] s2 = in.nextLine().split(" ");
+                System.out.println("Введите новое значение выбранного поля");
+                String s3 = in.next();
+                switch (s) {
+                    case "1" -> patientController.updateFirstNameByFullName(s2[1], s2[0], s2[2], s3);
+                    case "2" -> patientController.updateLastNameByFullName(s2[1], s2[0], s2[2], s3);
+                    case "3" -> patientController.updatePatronymicByFullName(s2[1], s2[0], s2[2], s3);
+                    case "4" -> patientController.updateAgeByFullName(s2[1], s2[0], s2[2], Integer.parseInt(s3));
+                    case "5" -> patientController.updateSexByFullName(s2[1], s2[0], s2[2], s3.equals("М") ? Sex.MALE : Sex.FEMALE);
+                }
             }
-        } else if (s1.equals("2")) {
-            System.out.println("Введите полное имя пациента в формате: Иванов Иван Иванович");
-            in.nextLine();
-            String[] s2 = in.nextLine().split(" ");
-            System.out.println("Введите новое значение выбранного поля");
-            String s3 = in.next();
-            switch (s) {
-                case "1" -> patientController.updateFirstNameByFullName(s2[1], s2[0], s2[2], s3);
-                case "2" -> patientController.updateLastNameByFullName(s2[1], s2[0], s2[2], s3);
-                case "3" -> patientController.updatePatronymicByFullName(s2[1], s2[0], s2[2], s3);
-                case "4" -> patientController.updateAgeByFullName(s2[1], s2[0], s2[2], Integer.parseInt(s3));
-                case "5" -> patientController.updateSexByFullName(s2[1], s2[0], s2[2], s3.equals("М") ? Sex.MALE : Sex.FEMALE);
-            }
+            System.out.println("Успешно!");
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Ввод некорректен!");
         }
-        System.out.println("Успешно!");
     }
 
     public static void main(String[] args) {
