d95050df682635d61e9c9705bc48db3ebaab99ed
Dmitry <iamdimonis@gmail.com>
2023-12-03 13:52:25 +0300

    delete useless methods

--- src/main/java/ru/vsu/csf/Main.java
+++ src/main/java/ru/vsu/csf/Main.java
@@ -3,13 +3,8 @@ package ru.vsu.csf;
 import ru.vsu.csf.command.*;
 import ru.vsu.csf.controller.DepartmentController;
 import ru.vsu.csf.controller.PatientController;
-import ru.vsu.csf.enums.Sex;
-import ru.vsu.csf.model.Department;
-import ru.vsu.csf.model.Patient;
 
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
 
